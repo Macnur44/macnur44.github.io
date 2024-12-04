f6a2186c78388768753b0c738ea9bdc74d316562
Macnur44 <macnur44@gmail.com>
2024-12-04 21:43:58 +0100

    Adicionar Scroll Suave no Menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('Olá Mundo');
+const linksInternos = document.querySelectorAll ('.js-menu a[href^="#"]');
+console.log (linksInternos);
+
+function initScrollSection (event){
+event.preventDefault ();
+const href = event.currentTarget.getAttribute ('href');
+const section = document.querySelector (href);
+
+section.scrollIntoView ({
+  behavior:'smooth',
+  block: 'start',
+})
+
+}
+
+linksInternos.forEach ((link)=>{
+  link.addEventListener ('click', initScrollSection);
+})
+
