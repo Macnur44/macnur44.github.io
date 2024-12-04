c9804adabb254ab0e2e5a78c497a79c1a3c72037
Macnur44 <macnur44@gmail.com>
2024-12-04 21:58:45 +0100

    actualizacao function initscroll

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-const linksInternos = document.querySelectorAll ('.js-menu a[href^="#"]');
-console.log (linksInternos);
-
-function initScrollSection (event){
-event.preventDefault ();
-const href = event.currentTarget.getAttribute ('href');
-const section = document.querySelector (href);
-
-section.scrollIntoView ({
-  behavior:'smooth',
-  block: 'start',
-})
-
+function initScrollSuave (){
+  const linksInternos = document.querySelectorAll ('.js-menu a[href^="#"]');
+  console.log (linksInternos);
+  
+  function initScrollSection (event){
+  event.preventDefault ();
+  const href = event.currentTarget.getAttribute ('href');
+  const section = document.querySelector (href);
+  
+  section.scrollIntoView ({
+    behavior:'smooth',
+    block: 'start',
+  })
+  
+  }
+  
+  linksInternos.forEach ((link)=>{
+    link.addEventListener ('click', initScrollSection);
+  })  
 }
-
-linksInternos.forEach ((link)=>{
-  link.addEventListener ('click', initScrollSection);
-})
-
+initScrollSuave ();
